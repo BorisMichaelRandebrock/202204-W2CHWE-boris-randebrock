c77693af1ea73dff96a3ad1979bc706ad204afb8
Boris <boris@randebrock.com>
2022-04-16 15:21:08 +0200

    created the container with theoretical positions of dead cells

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const cells = [];
-
+const positionedCells = [];
+const width = 3;
+const height = 3;
 class Cell {
   alive;
+  positionX;
+  positionY;
 
-  constructor() {
+  constructor(positionX, positionY) {
     this.alive = false;
+    this.positionX = positionX;
+    this.positionY = positionY;
   }
 }
 
-const cell = new Cell(false);
-cells.push(cell);
+const allocatedPositions = () => {
+  for (let y = 0; y <= height; y++) {
+    for (let x = 0; x <= width; x++) {
+      positionedCells.push(new Cell(x, y));
+    }
+  }
+  return positionedCells;
+};
+allocatedPositions();
