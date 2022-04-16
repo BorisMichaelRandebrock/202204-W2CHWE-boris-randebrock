aa164086f049d256b9f74ddfc9e8615bb5943791
Boris <boris@randebrock.com>
2022-04-16 19:18:10 +0200

    fix of test change

--- text_utils.test.js
+++ text_utils.test.js
@@ -13,28 +13,3 @@ describe("Given the addAAndB function", () => {
     });
   });
 });
-const allocatedPosition = [];
-const allocatedPositions = (row, column) => {
-  for (let y = 0; y < row; y++) {
-    allocatedPosition.push([]);
-    for (let x = 0; x < column; x++) {
-      allocatedPosition[y].push(new Cell(x, y));
-    }
-  }
-  return allocatedPosition;
-};
-
-describe("Given the function allocatedPosition", =>{
-  describe("When introduced the variables 2, 2", => {
-    test("Than it should return [[
-    Cell { positionX: 0, positionY: 0 },
-    Cell { positionX: 1, positionY: 0 }
-  ],
-  [
-    Cell { false, positionX: 0, positionY: 1 },
-    Cell { false, positionX: 1, positionY: 1 }
-  ]
-] ")
-  })
-})
-allocatedPositions()
