0719a7b4511a754f43ab47167a64202c03ba2e2b
Boris <boris@randebrock.com>
2022-04-17 17:26:29 +0200

    cornercases and rules of life

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class Cell {
     this.positionY = positionY;
   }
 }
-const firstLife = () => {
+const firstCellsAlive = () => {
   allocatedPosition[1][0].alive = true;
   allocatedPosition[1][1].alive = true;
   allocatedPosition[1][2].alive = true;
@@ -24,65 +24,91 @@ const positionDisplay = () => {
       allocatedPosition[y].push(new Cell(x, y));
     }
   }
-  firstLife();
+  firstCellsAlive();
   return allocatedPosition;
 };
 const grid = positionDisplay();
 
+const rulesOfLife = () => {
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < column; j++) {
+      if (positionDisplay[i][j].alive < 2 || positionDisplay[i][j] > 3) {
+        positionDisplay[i][j].alive = false;
+      }
+      if (positionDisplay[i][j] === 2 || positionDisplay[i][j] === 3) {
+        positionDisplay[i][j].alive = true;
+      }
+    }
+  }
+  return grid;
+};
 const checkNeighbours = () => {
   do {
     for (let i = 0; i < row; i++) {
       for (let j = 0; j < column; j++) {
         positionDisplay[i][j].alive = 0;
+        if (typeof positionDisplay[i - 1] !== "undefined") {
+          if (typeof positionDisplay[i - 1][j - 1] !== "undefined") {
+            if (positionDisplay[i - 1][j - 1].alive === true) {
+              positionDisplay[i][j].alive += 1;
+            }
+          }
+        }
+        if (typeof positionDisplay[i - 1] !== "undefined") {
+          if (typeof positionDisplay[i - 1][j] !== "undefined") {
+            if (positionDisplay[i - 1][j].alive === true) {
+              positionDisplay[i][j].alive += 1;
+            }
+          }
+        }
+        if (typeof positionDisplay[i - 1] !== "undefined") {
+          if (typeof positionDisplay[i - 1][j + 1] !== "undefined") {
+            if (positionDisplay[i - 1][j + 1].alive === true) {
+              positionDisplay[i][j].alive += 1;
+            }
+          }
+        }
+        if (typeof positionDisplay[i] !== "undefined") {
+          if (typeof positionDisplay[i][j - 1] !== "undefined") {
+            if (positionDisplay[i][j - 1].alive === true) {
+              positionDisplay[i][j].alive += 1;
+            }
+          }
+        }
 
-        if (positionDisplay[i - 1][j - 1].alive === true) {
-          positionDisplay[i][j].alive += 1;
+        if (typeof positionDisplay[i] !== "undefined") {
+          if (typeof positionDisplay[i][j + 1] !== "undefined") {
+            if (positionDisplay[i][j + 1].alive === true) {
+              positionDisplay[i][j].alive += 1;
+            }
+          }
         }
-        if (positionDisplay[i - 1][j].alive === true) {
-          positionDisplay[i][j].alive += 1;
+
+        if (typeof positionDisplay[i + 1] !== "undefined") {
+          if (typeof positionDisplay[i + 1][j - 1] !== "undefined") {
+            if (positionDisplay[i + 1][j - 1].alive === true) {
+              positionDisplay[i][j].alive += 1;
+            }
+          }
         }
-        if (positionDisplay[i - 1][j + 1].alive === true) {
-          positionDisplay[i][j].alive += 1;
+        if (typeof positionDisplay[i + 1] !== "undefined") {
+          if (typeof positionDisplay[i + 1][j] !== "undefined") {
+            if (positionDisplay[i + 1][j].alive === true) {
+              positionDisplay[i][j].alive += 1;
+            }
+          }
         }
-        if (positionDisplay[i][j - 1].alive === true) {
-          positionDisplay[i][j].alive += 1;
-        }
-        if (positionDisplay[i][j + 1].alive === true) {
-          positionDisplay[i][j].alive += 1;
-        }
-        if (positionDisplay[i + 1][j - 1].alive === true) {
-          positionDisplay[i][j].alive += 1;
-        }
-        if (positionDisplay[i + 1][j].alive === true) {
-          positionDisplay[i][j].alive += 1;
-        }
-        if (positionDisplay[i + 1][j + 1].alive === true) {
-          positionDisplay[i][j].alive += 1;
-        }
-        if (
-          positionDisplay[i][j].alive === false &&
-          positionDisplay[i][j].alive > 2
-        ) {
-          positionDisplay[i][j].alive = true;
-        } else if (
-          positionDisplay[i][j].alive === true &&
-          positionDisplay[i][j].alive === 2
-        ) {
-          positionDisplay[i][j].alive = true;
-        } else if (
-          positionDisplay[i][j].alive === true &&
-          positionDisplay[i][j].alive === 3
-        ) {
-          positionDisplay[i][j].alive = true;
-        } else {
-          positionDisplay[i][j].alive = false;
+        if (typeof positionDisplay[i + 1] !== "undefined") {
+          if (typeof positionDisplay[i + 1][j + 1] !== "undefined") {
+            if (positionDisplay[i + 1][j + 1].alive === true) {
+              positionDisplay[i][j].alive += 1;
+            }
+          }
         }
       }
     }
   } while (positionDisplay.alive === false);
-  return grid;
+  rulesOfLife();
 };
 
 checkNeighbours();
-
-// console.log(grid);
